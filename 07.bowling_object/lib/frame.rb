57ec870edfb7dcd1c6219ca0f7aebaa7f60fd135
R-Tsukada <r.tsukada14@gmail.com>
2021-05-29 09:29:15 +0900

    1フレームごとのスコア計算をするクラスを追加

---
+++ 07.bowling_object/lib/frame.rb
@@ -0,0 +1,27 @@
+
+# frozen_string_literal: true
+
+require './lib/mark'
+
+# frame
+class Frame
+  attr_reader :first_mark, :second_mark, :third_mark
+
+  def initialize(first_mark, second_mark, third_mark)
+    @first_mark = Mark.new(first_mark)
+    @second_mark = Mark.new(second_mark)
+    @third_mark = Mark.new(third_mark)
+  end
+
+  def score
+    @first_mark.score + @second_mark.score + @third_mark.score
+  end
+
+  def strike?
+    @first_mark.score == 10
+  end
+
+  def spare?
+    !strike? && [@first_mark.score, @second_mark.score].sum == 10
+  end
+end
