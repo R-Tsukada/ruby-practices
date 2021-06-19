881b7a48e1a4e7f1a0c2077f7e62a938d867384d
R-Tsukada <r.tsukada14@gmail.com>
2021-06-19 20:43:31 +0900

    テストを作成

---
+++ 08.ls_object/test/test_ls_command.rb
@@ -0,0 +1,59 @@
+require 'minitest/autorun'
+require './lib/file_list'
+require './lib/short_style'
+require 'pathname'
+
+class LsCommandTest < Minitest::Test
+  TARGET_PATHNAME = Pathname('test/fixture/fjord-books_app')
+
+  def test_run_ls
+    expected = <<~TEXT.chomp
+      Gemfile                 config                  postcss.config.js
+      Gemfile.lock            config.ru               public
+      README.md               db                      storage
+      Rakefile                lib                     test
+      app                     log                     tmp
+      babel.config.js         node_modules            yarn.lock
+      bin                     package.json
+    TEXT
+    file_list = FileList.new(TARGET_PATHNAME)
+    assert_equal expected, ShortStyle.new(file_list).run_ls
+  end
+
+  def run_ls_show_dots
+    expected = <<~TEXT.chomp
+      .                       Gemfile.lock            node_modules
+      ..                      README.md               package.json
+      .browserslistrc         Rakefile                postcss.config.js
+      .env                    app                     public
+      .env.sample             babel.config.js         storage
+      .git                    bin                     test
+      .gitignore              config                  tmp
+      .idea                   config.ru               vendor
+      .rubocop.yml            db                      yarn.lock
+      .ruby-version           lib
+      Gemfile                 log
+    TEXT
+    file_list = FileList.new(TARGET_PATHNAME, dot_match: true)
+    assert_equal expected, ShortStyle.new(file_list).run_ls
+  end
+
+  def run_ls_reverse
+    expected = <<~TEXT.chomp
+      yarn.lock               package.json            bin
+      vendor                  node_modules            babel.config.js
+      tmp                     log                     app
+      test                    lib                     Rakefile
+      storage                 db                      README.md
+      public                  config.ru               Gemfile.lock
+      postcss.config.js       config                  Gemfile
+    TEXT
+    file_list = FileList.new(TARGET_PATHNAME, reverse: true)
+    assert_equal expected, ShortStyle.new(file_list).run_ls
+  end
+
+  def test_run_ls_long_format
+    expected = `ls -l #{TARGET_PATHNAME}`.chomp
+    assert_equal expected, ShortStyle.new(file_list, long_format: true).run_ls
+  end
+end
