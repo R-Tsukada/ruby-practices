49b7620d2b8ae6bf165fca401a2f1d93179f3cea
R-Tsukada <r.tsukada14@gmail.com>
2021-06-27 21:14:20 +0900

    ファイル情報の取得

---
+++ 08.ls_object/lib/file_data.rb
@@ -0,0 +1,60 @@
+class FileData
+  MODE_TABLE = {
+    '0' => '---',
+    '1' => '-x-',
+    '2' => '-w-',
+    '3' => '-wx',
+    '4' => 'r--',
+    '5' => 'r-x',
+    '6' => 'rw-',
+    '7' => 'rwx'
+  }.freeze
+
+  def initialize(filepath)
+    @filepath = filepath
+    @file_stat = File::Stat.new(@filepath)
+    @file_name = File.basename(@filepath)
+  end
+
+  def file_blocks
+    @file_stat.blocks
+  end
+
+  def nlink
+    @file_stat.nlink.to_s
+  end
+
+  #変数名を変更する
+  def file_owner
+    Etc.getpwuid(@file_stat.uid).name
+  end
+
+  #変数名を変更する
+  def file_group
+    Etc.getgrgid(@file_stat.gid).name
+  end
+
+  def bytesize
+    @file_stat.size.to_s
+  end
+
+  def mtime
+    @file_stat.mtime.strftime('%_m %e %R')
+  end
+
+  def type
+    case @file_stat.ftype
+    when 'directory'
+      'd'
+    when 'link'
+      'l'
+    else
+      '-'
+    end
+  end
+
+  def mode
+    digits = @file_stat.mode.to_s(8)[-3..]
+    digits.gsub(/./, MODE_TABLE)
+  end
+end
--- 08.ls_object/lib/file_list.rb
+++ 08.ls_object/lib/file_list.rb
@@ -1,6 +1,9 @@
 class FileList
-  def initialize(pathname)
+  def initialize(pathname, reverse: false, dot_match: false)
     @pathname = pathname.glob('*').sort
+    @reverse = reverse
+    @dot_match = dot_match
+    @files = initialize_files
   end
 
   def filename
@@ -9,5 +12,21 @@ class FileList
 
   def max_filename_count
     @pathname.map { |f| File.basename(f).size }.max
+  end
+
+  def total_file_blocks
+    @files.sum(&:file_blocks)
+  end
+
+  def initialize_files
+    file_paths = collect_file_paths
+    file_paths.map { |file_path| FileData.new(file_path)}
+  end
+
+  def collect_file_paths
+    pattern = @pathname.join('*')
+    params = @dot_match ? [pattern, File::FMN_DOTMATCH] : [pattern]
+    file_paths = Dir.glob(*params).sort
+    @reverse ? file_paths.reverse : file_paths
   end
 end
