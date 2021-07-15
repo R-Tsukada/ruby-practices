36ad90ea0feb8c271e1eb5cab25b0bfe3a133e52
R-Tsukada <r.tsukada14@gmail.com>
2021-07-15 10:26:12 +0900

    クラスから外れていたメソッドをクラス内に移動

--- 07.bowling_object/lib/game.rb
+++ 07.bowling_object/lib/game.rb
@@ -23,22 +23,21 @@ class Game
     end
     score
   end
-end
 
-def next_frame(current_number)
-  @frames.fetch(current_number + 1, nil)
-end
-
-def frames(marks)
-  frame_count = 0
-  frames = []
-  10.times do |n|
-    first_mark = marks[frame_count]
-    second_mark = marks[frame_count += 1] if first_mark != 10 || n == 9
-    third_mark = marks[frame_count += 1] if n == 9
-    frames << Frame.new(first_mark, second_mark, third_mark)
-    frame_count += 1
+  def next_frame(current_number)
+    @frames.fetch(current_number + 1, nil)
   end
-  frames
-end
 
+  def frames(marks)
+    frame_count = 0
+    frames = []
+    10.times do |n|
+      first_mark = marks[frame_count]
+      second_mark = marks[frame_count += 1] if first_mark != 10 || n == 9
+      third_mark = marks[frame_count += 1] if n == 9
+      frames << Frame.new(first_mark, second_mark, third_mark)
+      frame_count += 1
+    end
+    frames
+  end
+end
